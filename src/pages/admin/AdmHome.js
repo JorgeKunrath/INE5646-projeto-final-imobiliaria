42bdc0917b034938afc93a21323f1e965ea46391
Jorge Kunrath <jorgeks95@gmail.com>
2020-11-25 23:42:22 -0300

    cossitas

--- src/pages/admin/AdmHome.js
+++ src/pages/admin/AdmHome.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import AdmNavExample from '../../components/AdmNavExample'
 
 export default function AdmHome() {
   return (
     <div>
+      <AdmNavExample />
       <h1>Admin - home</h1>
     </div>
   )
