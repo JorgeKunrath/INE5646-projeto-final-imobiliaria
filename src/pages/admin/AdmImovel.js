1a38b2ac2d19817b2d7ec86c2aacd66cd910139e
Jorge Kunrath <jorgeks95@gmail.com>
2020-12-01 02:41:22 -0300

    setup react hook form, lindo demais :heart_eyes_cat:

--- src/pages/admin/AdmImovel.js
+++ src/pages/admin/AdmImovel.js
@@ -2,14 +2,38 @@ import React from 'react'
 
 import HeaderAdmin from '../../components/common/HeaderAdmin'
 import Footer from '../../components/common/Footer'
+import AdmFormExample from '../../components/AdmFormExample'
 
 export default function AdmImovel() {
+  const [dbData, setDbData] = React.useState(null)
+
+  React.useEffect(() => {
+    setTimeout(
+      () =>
+        setDbData({
+          titulo: 'placeholder titulo mas n exatamente',
+          inscricaoMunicipal: 'inscricao q ja veio do banco de dados',
+          aluguel: 123,
+          codigoProvisorio: 321,
+        }),
+      500
+    )
+  }, [])
+
   return (
     <>
       <HeaderAdmin />
       <h1>
         Admin - imovel (criação e edição, por enquanto, e talvez pra sempre)
       </h1>
+
+      <main>
+        {dbData ? (
+          <AdmFormExample dbData={dbData} />
+        ) : (
+          <p style={{ textAlign: 'center' }}>Carregando dados</p>
+        )}
+      </main>
 
       <Footer />
     </>
