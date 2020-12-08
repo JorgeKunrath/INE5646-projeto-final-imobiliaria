4f6bfe0ee7528b287d8baa02814672ec840f9c87
Jorge Kunrath <jorgeks95@gmail.com>
2020-12-08 02:32:46 -0300

    ajustes visuais

--- src/components/common/HeaderFront.js
+++ src/components/common/HeaderFront.js
@@ -2,36 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import IcoUser from '../../icons/User'
 import SiteContainer from './SiteContainer'
 
 const HeaderInner = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
   min-height: 72px;
-
-  img {
-    padding-right: 1em;
-  }
-
-  .tel {
-    display: block;
-    margin-left: auto;
-    padding: 0.5em 0.75em;
-    text-decoration: none;
-    text-align: center;
-    line-height: 1;
-    background: var(--green);
-    color: white;
-    border-radius: 5px;
-    box-shadow: var(--shadow);
-  }
-
-  svg {
-    margin-left: 1em;
-    box-shadow: var(--shadow);
-    border-radius: 50%;
-  }
 `
 
 export default function HeaderFront() {
@@ -45,10 +22,6 @@ export default function HeaderFront() {
               alt="Logo Genérico"
             />
           </Link>
-          <a href="tel:48999999999" className="tel">
-            48 99999 9999
-          </a>
-          <IcoUser size={40} />
         </HeaderInner>
       </SiteContainer>
     </header>
