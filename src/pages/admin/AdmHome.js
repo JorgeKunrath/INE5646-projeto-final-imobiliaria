2e7363a267b73576d1cded12729fe99ec3a59576
JorgeKunrath <54911848+JorgeKunrath@users.noreply.github.com>
2020-12-09 00:19:36 -0300

    Pagina imoveis cadastrados (#14)
    * novas cores
    
    * home do admin — imóveis cadastrados

--- src/pages/admin/AdmHome.js
+++ src/pages/admin/AdmHome.js
@@ -1,14 +1,45 @@
 import React from 'react'
 
+import { getUserHouses } from '../../services/firestore'
 import HeaderAdmin from '../../components/common/HeaderAdmin'
 import Footer from '../../components/common/Footer'
+import TableHouses from '../../components/admin/table/TableHouses'
+import SiteContainer from '../../components/common/SiteContainer'
 
 export default function AdmHome() {
+  const [loaded, setLoaded] = React.useState(false)
+  const [data, setData] = React.useState(null)
+  const [filteredData, setFilteredData] = React.useState(null)
+
+  // request pro banco de dados
+  React.useEffect(() => {
+    getUserHouses(setData)
+  }, [])
+
+  // trata os dados (não implementado)
+  React.useEffect(() => {
+    setLoaded(false)
+    if (data) {
+      setFilteredData(data)
+      setLoaded(true)
+    }
+  }, [data])
+
   return (
     <>
       <HeaderAdmin />
-      <h1>Admin - home</h1>
-
+      <SiteContainer>
+        <h1
+          style={{
+            margin: '2rem 0 0',
+            fontSize: '2rem',
+            color: 'var(--gray3)',
+          }}
+        >
+          Imóveis Cadastrados
+        </h1>
+      </SiteContainer>
+      <TableHouses data={filteredData} loaded={loaded} />
       <Footer />
     </>
   )
