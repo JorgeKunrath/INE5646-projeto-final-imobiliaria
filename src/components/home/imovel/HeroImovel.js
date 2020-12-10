d54e4248e9abb492ad5c2aaa0a784ce24a698132
Jorge Kunrath <jorgeks95@gmail.com>
2020-12-10 17:39:39 -0300

    ui hero imovel

--- src/components/home/imovel/HeroImovel.js
+++ src/components/home/imovel/HeroImovel.js
@@ -4,22 +4,30 @@ import styled from 'styled-components'
 const Wrapper = styled.section`
   /* border: 3px solid red; */
   background-color: var(--black);
+  position: relative;
+  min-height: 60vmin;
 
-  > div {
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  /* > div {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  }
+  } */
 `
 
 export default function HeroImovel({ images }) {
+  console.log({ images })
   return (
     <Wrapper>
       {/* TODO finalizar carrossel de fotos */}
-      <div>
-        {images.map((img, i) => {
-          return <img key={img} src={img} alt="" />
-        })}
-      </div>
+      <img src={images[0]} alt="" />
     </Wrapper>
   )
 }
