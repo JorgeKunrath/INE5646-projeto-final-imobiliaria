91791930db2ef0fcc9f50eaba525840237c8dcbb
JorgeKunrath <54911848+JorgeKunrath@users.noreply.github.com>
2020-12-13 18:13:09 -0300

    carrosselzin 🎠 🎠 🎠 (#21)

--- src/components/home/imovel/HeroImovel.js
+++ src/components/home/imovel/HeroImovel.js
@@ -1,32 +1,60 @@
 import styled from 'styled-components'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
 
 const Wrapper = styled.section`
-  /* border: 3px solid red; */
   background-color: var(--black);
-  position: relative;
-  min-height: 60vmin;
 
   img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    max-height: 70vmin;
+    object-fit: contain;
   }
 
-  /* > div {
+  /**
+  * overwrites carousel
+  */
+  .carousel .slider .slide {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  } */
+    place-items: center;
+  }
+  .carousel.carousel-slider .control-arrow {
+    width: 70px;
+  }
+  .carousel .control-dots {
+    margin: 0.25rem auto;
+    display: flex;
+    justify-content: center;
+    height: 1rem;
+  }
+  .carousel .control-dots .dot {
+    opacity: 0.6;
+    width: 0.5rem;
+    height: 0.5rem;
+    display: block;
+    position: relative;
+
+    /* make clickable area bigger */
+    ::after {
+      content: '';
+      position: absolute;
+      width: 1rem;
+      height: 1rem;
+      top: -50%;
+      left: -50%;
+    }
+  }
+  .carousel .control-dots .dot.selected {
+    opacity: 1;
+    transform: scale(1.2);
+  }
 `
 
 export default function HeroImovel({ images }) {
-  console.log({ images })
   return (
     <Wrapper>
-      {/* TODO finalizar carrossel de fotos */}
-      <img src={images[0]} alt="" />
+      <Carousel showThumbs={false} showStatus={false} infiniteLoop={true}>
+        {images && images.map((image, i) => <img key={i} src={image} alt="" />)}
+      </Carousel>
     </Wrapper>
   )
 }
